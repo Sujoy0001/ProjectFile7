eb2f9ab1f1193c2a72c77f09337e97e397cf054c
Tarapada Garai <146563644+codingWithRakesh@users.noreply.github.com>
2025-07-06 13:17:47 +0530

    Add smooth scrolling effect to multiple pages and enable MongoDB connection logging

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -4,7 +4,7 @@ import {DB_NAME} from "../constants.js"
 const connectDB = async () => {
     try {
         const connectionIn = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        // console.log("mongoDB connected",connectionIn.connection.host)
+        console.log("mongoDB connected",connectionIn.connection.host)
     } catch (error) {
         console.log("MongoDB Failed",error?.message)
         process.exit(1)
